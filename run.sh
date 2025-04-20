69a55b41493223050d351f854515e26bf2f53d8c
Jeff Bencin <jeff.bencin@gmail.com>
2025-04-20 19:06:58 -0400

    Use `xargs` to run multi-threaded

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 set -euo pipefail
 
+threads=8
 dl_dir="./dl"
 contracts_repo_dir="$dl_dir/clarity-deployed-contracts"
 
@@ -11,23 +12,20 @@ echo "Getting latest version of deployed contracts..."
 if [ -d "$contracts_repo_dir" ] ; then
   pushd "$contracts_repo_dir" > /dev/null
   git reset --hard HEAD
-  git pull
+  git pull --quiet > /dev/null
   popd > /dev/null
 else
-  git clone git@github.com:boomcrypto/clarity-deployed-contracts.git "$contracts_repo_dir"
+  git clone --quiet git@github.com:boomcrypto/clarity-deployed-contracts.git "$contracts_repo_dir"
 fi
 
 echo "Checking contracts in $contracts_repo_dir before formatting..."
-find "$contracts_repo_dir/contracts" -type f -print0 | while IFS= read -r -d '' file; do
-  clarinet check "$file" > /dev/null || echo "❌ check failed on file pre-format: $file"
-done
+find "$contracts_repo_dir/contracts" -type f -print0 \
+  | xargs -0 -P "$threads" -I % sh -c 'clarinet check % &> /dev/null || echo "❌ check failed on file pre-format: %"'
 
 echo "Formatting contracts in $contracts_repo_dir..."
-find "$contracts_repo_dir/contracts" -type f -print0 | while IFS= read -r -d '' file; do
-  clarinet format --in-place --file "$file" > /dev/null || echo "❌ failed to format file: $file"
-done
+find "$contracts_repo_dir/contracts" -type f -print0 \
+  | xargs -0 -P "$threads" -I % sh -c 'clarinet format --in-place --file % &> /dev/null || echo "❌ failed to format file: %"'
 
 echo "Checking contracts in $contracts_repo_dir after formatting..."
-find "$contracts_repo_dir/contracts" -type f -print0 | while IFS= read -r -d '' file; do
-  clarinet check "$file" > /dev/null || echo "❌ check failed on file post-format: $file"
-done
+find "$contracts_repo_dir/contracts" -type f -print0 \
+  | xargs -0 -P "$threads" -I % sh -c 'clarinet check % &> /dev/null || echo "❌ check failed on file post-format: %"'
